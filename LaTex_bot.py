d590d7a657f24df433f8d7caec4179248870823c
Dmitry Maltsov <47698240+dmitry-maltsov@users.noreply.github.com>
2020-02-21 00:10:55 +0300

    issue#4, sprint#1, Done, Добавлено приветствие и нулевой интерфейс

---
+++ LaTex_bot.py
@@ -0,0 +1,18 @@
+import telebot
+
+from conf import Config
+from ans import Answers
+
+
+bot_token = Config.get_token()
+
+bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    """Reply with welcome message and main keyboard"""
+    bot.send_message(message.chat.id, Answers.start_ans, reply_markup=Answers.main_markup, parse_mode='markdown')
+
+
+bot.polling()
