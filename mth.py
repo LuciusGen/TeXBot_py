3d3fae8228acfc0217d9e512ea43172c5770cde2
FirsovaEA <FirsovaEA@users.noreply.github.com>
2020-02-24 20:42:24 +0300

    issue#10, sprint#1, Done, Добавлены названия разделов

--- mth.py
+++ mth.py
@@ -3,14 +3,14 @@ from telebot import types
 
 class Math:
 
-    section_1_button = types.InlineKeyboardButton("Раздел 1",
+    section_1_button = types.InlineKeyboardButton("Математический анализ",
                                                   callback_data="SEC_1")
-    section_2_button = types.InlineKeyboardButton("Раздел 2",
+    section_2_button = types.InlineKeyboardButton("Линейная алгебра",
                                                   callback_data="SEC_2")
-    section_3_button = types.InlineKeyboardButton("Раздел 3",
+    section_3_button = types.InlineKeyboardButton("Комплексный анализ",
                                                   callback_data="SEC_3")
 
-    math_markup = types.InlineKeyboardMarkup()
+    math_markup = types.InlineKeyboardMarkup(row_width=1)
     math_markup.add(section_1_button, section_2_button, section_3_button)
 
     math_ans = "Выберите раздел:"
