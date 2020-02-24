6a73417b4667c357770c8aa5026a7cfa715e5c28
FirsovaEA <FirsovaEA@users.noreply.github.com>
2020-02-24 19:33:10 +0300

    issue#10, sprint#1, Done, Созданы кнопки с выбором разделов

--- LaTex_bot.py
+++ LaTex_bot.py
@@ -2,6 +2,7 @@ import telebot
 
 from conf import Config
 from ans import Answers
+from mth import Math
 
 
 bot_token = Config.get_token()
@@ -13,6 +14,12 @@ bot = telebot.TeleBot(bot_token)
 def start_message(message):
     """Reply with welcome message and main keyboard"""
     bot.send_message(message.chat.id, Answers.start_ans, reply_markup=Answers.main_markup, parse_mode='markdown')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == '📚Вышмат':
+        bot.send_message(message.chat.id, Math.math_ans, reply_markup=Math.math_markup)
 
 
 bot.polling(none_stop=False, interval=1, timeout=20)
