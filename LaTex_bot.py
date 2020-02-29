fc07d3f54520dce6dcf4786ec5350a25f97dd190
Dmitry Maltsov <47698240+dmitry-maltsov@users.noreply.github.com>
2020-02-29 18:03:06 +0300

    issue#7, sprint#1, Done, Добавление заглушки для команды /tex

--- LaTex_bot.py
+++ LaTex_bot.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import telebot
+from telebot import types
+import re
+
 import mth
 from conf import Config
 from ans import Answers
 from mth import Math
-from telebot import types
+
 
 url_donate_path = 'https://money.yandex.ru/to/4100111962148422'
 url_team_leader = 'https://t.me/dont_open'
@@ -16,6 +19,18 @@ bot = telebot.TeleBot(bot_token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, Answers.start_ans, reply_markup=Answers.main_markup, parse_mode='markdown')
+
+
+@bot.message_handler(regexp=r"^\/tex .+")
+def convert_latex(message):
+    regex = r"^\/tex (.+)"
+    parser = re.search(regex, message.text)
+    tex_command = parser.group(1)
+    
+    # Заглушка. Убрать, когда сделаем генерацию картинки
+    bot.send_message(message.chat.id,
+                     "Вы ввели команду *\"" + tex_command + "\"*. Вскоре я научусь переводить её в картинку!",
+                     parse_mode='markdown')
 
 
 @bot.message_handler(content_types=['text'])
